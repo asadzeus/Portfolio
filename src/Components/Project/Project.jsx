c648c353cb5696f6332c91c94c78c4786803e8e8
alper <86222517+asadzeus@users.noreply.github.com>
2022-07-26 10:31:00 +0300

    s

--- src/Components/Project/Project.jsx
+++ src/Components/Project/Project.jsx
@@ -2,7 +2,7 @@ import './Project.css'
 
 export default function Project({link,name,img}) {
   return (
-    <div className='project'><a href={link}>
+    <div className='project'><a href={link} className="project-link">
         <div className="title-container">
           <h1 className="website-title">{name}</h1>
         </div>
